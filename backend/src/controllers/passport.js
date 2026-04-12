18b9a09e501cec9fd15f195ccb927ce396f46b16
manh <manh99294@donga.edu.vn>
2026-04-13 01:41:06 +0700

    update

--- backend/src/controllers/passport.js
+++ backend/src/controllers/passport.js
@@ -1,6 +1,5 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const FacebookStrategy = require("passport-facebook").Strategy;
 const User = require("../models/User");
 
 passport.use(
