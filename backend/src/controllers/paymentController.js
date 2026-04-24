bc40457405a509bb603cfac568ce2cc6622e84ac
manh <manh99294@donga.edu.vn>
2026-04-24 23:49:17 +0700

    update

--- backend/src/controllers/paymentController.js
+++ backend/src/controllers/paymentController.js
@@ -8,13 +8,18 @@ const OrderController = require("./orderController");
 
 function sortObject(obj) {
   let sorted = {};
-  let keys = Object.keys(obj).sort();
-
-  keys.forEach((key) => {
-    sorted[key] = obj[key];
-  });
-
-  return sorted;
+    let str = [];
+    let key;
+    for (key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        str.push(encodeURIComponent(key));
+      }
+    }
+    str.sort();
+    for (key = 0; key < str.length; key++) {
+      sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+    }
+    return sorted;
 }
 
 // USER GET PAYMENTS
@@ -143,6 +148,7 @@ console.log("SECURE HASH FROM VNPAY:", secureHash);
 
 // remove để ký lại
 delete rawParams["vnp_SecureHash"];
+delete rawParams["vnp_SecureHashType"];
 
 const sortedParams = sortObject(rawParams);
 
